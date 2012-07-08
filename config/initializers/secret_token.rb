bf81b6e3ce3fd9ae6cbf3eb857eb74352b0a5ee8
Dimitar Dimitrov <me@ddimitrov.name>
2012-07-08 20:22:50 +0300

    Default Rails 3.2.6 app on Ruby 1.8.7

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsOnSpace::Application.config.secret_token = '1faf9f3038aa82a70537b11287f4d070e5631b40acb50e31ae97b4031eb20334709025555f8aae442b8e7103d528ee7045213f8635fd58988613a1186a4045f5'
