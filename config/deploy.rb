8898cb024dda87dd8ecd4f732ce6dbcd09a14ed5
Dimitar Dimitrov <me@ddimitrov.name>
2012-07-27 15:41:34 +0300

    Space.bg specific Capistrano deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,11 +2,20 @@ set :application,   'ror.space.bg'
 set :repository,    'git://github.com/mitio/ror.space.bg.git'
 set :scm,           :git
 set :deploy_via,    :remote_cache
+set :user,          'rorspace'
+set :deploy_to,     "/home/#{user}/#{application}"
 set :use_sudo,      false
 
 role :web, "#{application}"
 role :app, "#{application}"
 role :db,  "#{application}", :primary => true
+
+# Simulate a TTY via the pseudo-TTY feature of Capistrano.
+default_run_options[:pty] = true
+
+# Don't try to set the mtime of the static files in public.
+# Remove this line if you're not using the new Rails 3.1 asset pipeline.
+set :normalize_asset_timestamps, false
 
 namespace :deploy do
   task :start do ; end
